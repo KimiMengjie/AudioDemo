dcad12c89986910b616839f78cc9fe2f229cdac1
kimi <mengjiechen1989@icloud.com>
2016-10-06 22:32:38 +0800

    生成lame静态库，添加录音后将音频文件压缩成MP3然后删除源文件。

--- 音频录制/录音功能实现/录音功能实现/ViewController.swift
+++ 音频录制/录音功能实现/录音功能实现/ViewController.swift
@@ -14,10 +14,20 @@ class ViewController: UIViewController {
     lazy var record: AVAudioRecorder? = {
         
         //开始录音
-        let url = NSURL(string: "/Users/kimi/Desktop/audio/test.caf") as! URL
+        let url = NSURL(string: "/Users/kimi/Desktop/test.caf") as! URL
         
         //setting:录音的设置项
-        let configDic = ["":""]
+        //录音参数设置(不需要掌握，一些固定的配置)
+        let configDic:[String: AnyObject] = [
+            //编码格式
+            AVFormatIDKey: NSNumber(integerLiteral: Int(kAudioFormatLinearPCM)),
+            //采样率
+            AVSampleRateKey: NSNumber(floatLiteral: 11025.0),
+            //通道数
+            AVNumberOfChannelsKey:NSNumber(integerLiteral: 2),
+            //录音质量
+            AVEncoderAudioQualityKey:NSNumber(integerLiteral: AVAudioQuality.min.rawValue)
+        ]
         
         do {
             let record = try AVAudioRecorder(url: url, settings: configDic)
@@ -60,13 +70,16 @@ class ViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         //开始录音
         print("开始录音")
+    
         record?.record()
+        //指定录音3秒
+//        record?.record(forDuration: 3)
+//        record?.record(atTime: (record?.currentTime)! + 2, forDuration: 3)
     }
     
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         //结束录音
-        
         //根据当前的录音时间，做处理
         //如果录音不超过两秒，则删除录音
         //如果超过，就正常处理
@@ -75,12 +88,18 @@ class ViewController: UIViewController {
         }else{
             print("录音时间太短")
             //删除录音
-            //如果想要删除录音文件
+            //如果想要删除录音文件，必须先让录音文件停止
+            //但是不进行编码的话4秒就900多KB
+            record?.stop()
             record?.deleteRecording()
         }
         
         print("结束录音")
         record?.stop()
+        
+        //转码,转成功后删除源文件
+        let path = lameTool.audio(toMP3: "/Users/kimi/Desktop/test.caf", isDeleteSourchFile: true)
+        print(path)
     }
 
 
